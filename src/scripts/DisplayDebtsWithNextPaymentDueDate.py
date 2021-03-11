ed41ff94621cd8485b3aedd5c1057506cdae7cc0
Anoh Kadjo <kadjoclement@gmail.com>
2021-03-10 23:05:15 -0600

    Customer-Debts-DisplayDebtsWithNextPaymentDueDate-Script
    Created script DisplayDebtsWithNextPaymentDueDate to return all debt objects with the next payment due date field. The script has a method displayDebtsWithNextPaymentDueDate that takes a list of debt objects that  have the remaining balance field on them, and a custom dict structure I created to hold the payment plan per debtId.

---
+++ src/scripts/DisplayDebtsWithNextPaymentDueDate.py
@@ -0,0 +1,38 @@
+
+from Utils import Utils
+import logging
+import os
+
+debts = []
+
+logger = logging.getLogger(__name__)
+logger.setLevel(level=logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+log_dir_path = os.path.join(os.getcwd(), 'src')
+
+def displayDebtsWithNextPaymentDueDate(debts_with_remainingBalance, debts_and_payments):
+    for debt in debts_with_remainingBalance:
+        debt = Utils.calculateNextPaymentDueDate(debt, debts_and_payments)
+        debts.append(debt)
+        print(debt)
+
+def getDebtsWithNextPaymentDueDate():
+    return debts 
+          
+def main(debts_with_remainingBalance, debts_and_payments):
+    handler = logging.FileHandler(log_dir_path+'/Scripts.log', 'w+')
+    handler.setLevel(logging.INFO)
+    handler.setFormatter(formatter)
+    # console log
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    console.setFormatter(formatter)
+
+    logger.addHandler(handler)
+    logger.addHandler(console)
+
+    displayDebtsWithNextPaymentDueDate(debts_with_remainingBalance, debts_and_payments)
+
+if __name__ == "__main__":
+    "This script will be called in Main.py"
